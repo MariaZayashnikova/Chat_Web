f7c7050eb135bfaadacc6ac7f67af698bceb9767
Mariya_Zaya <mariyazayaschnikova@gmail.com>
2023-08-03 18:23:20 +0300

    refactoring chat

--- src/components/pages/Operator-page/Chat/Chat.js
+++ src/components/pages/Operator-page/Chat/Chat.js
@@ -9,58 +9,20 @@ import { updateChatsInDB, pushNewMessage } from '../../../../actions'
 import { calculateDate, CalcStars } from '../../../../utils'
 import OperatorAnswer from './OperatorAnswer'
 import PicturePreview from '../../../Picture-preview/Picture-preview'
+import Spinner from '../../../Spinner/Spinner'
+import ErrorRoute from '../../../ErrorRoute/ErrorRoute'
 import './Chat.css'
 
-function Chat({ chats, updateChatsInDB, settingsUser, pushNewMessage, user }) {
-
-    let messageArray = [],
+function Chat({ chats, updateChatsInDB, settingsUser, pushNewMessage, user, loading }) {
+    let chatMessages = [],
         clientName,
-        isSavedCase = false,
-        dialogueResultObj = {}
+        isSavedChat = false,
+        chatResult = {}
 
     let { itemId } = useParams()
-    //сделать обработку несуществующего диалога через url
-    function filterData() {
-        for (let dialogueObj in chats) {
-            if (dialogueObj !== itemId) continue
-
-            let dialogue = chats[dialogueObj]
-
-            if (dialogue.status === 'active') {
-                updateChatsInDB({ status: 'inWork', operatorUID: user.uid }, itemId)
-                break
-            }
-            clientName = dialogue.client
-
-            if (dialogue.status) {
-                dialogueResultObj.completionTime = dialogue.completionTime
-                dialogueResultObj.grade = dialogue.grade
-            }
-
-            if (dialogue.isSave) {
-                isSavedCase = dialogue.isSave
-            }
-
-            let messages = dialogue.messages
-
-            for (let messageTime in messages) {
-                let contentMessage = messages[messageTime]
-                let obj = {
-                    time: parseInt(messageTime, 10),
-                    content: contentMessage.content,
-                    isOperator: contentMessage.isOperator,
-                }
-                messageArray.push(obj)
-            }
-
-            break;
-        }
-    }
 
     function checkIsFirstMessage() {
-        if (!messageArray || !settingsUser.automaticGreeting) return
-
-        if (messageArray.some(elem => elem.isOperator)) return
+        if (chatMessages.some(elem => elem.isOperator) || !settingsUser.automaticGreeting) return
 
         let time = new Date().getTime()
         let newMessage = {
@@ -72,50 +34,85 @@ function Chat({ chats, updateChatsInDB, settingsUser, pushNewMessage, user }) {
         pushNewMessage(newMessage, itemId)
     }
 
-    if (chats) {
-        filterData()
-        checkIsFirstMessage()
+    function filterData() {
+        for (let chatId in chats) {
+            if (chatId !== itemId) continue
+
+            const chat = chats[chatId]
+
+            if (chat.status === 'active') {
+                updateChatsInDB({ status: 'inWork', operatorUID: user.uid }, itemId)
+                break
+            }
+            clientName = chat.client
+
+            if (chat.status === 'finished') {
+                chatResult.completionTime = chat.completionTime
+                chatResult.grade = chat.grade
+            }
+
+            if (chat.isSave) isSavedChat = chat.isSave
+
+            const messages = chat.messages
+
+            for (let messageTime in messages) {
+                let message = messages[messageTime]
+                let obj = {
+                    time: parseInt(messageTime, 10),
+                    content: message.content,
+                    isOperator: message.isOperator,
+                }
+                chatMessages.push(obj)
+            }
+            checkIsFirstMessage()
+            break;
+        }
     }
 
-    function analyzeContent(str) {
-        let options = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'svg'],
-            res,
+    if (chats) {
+        filterData()
+        if (chatMessages.length === 0) return < ErrorRoute />
+    }
+
+    function getImageConfig(str) {
+        const options = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'svg']
+        let image = false,
             index,
-            startSrt,
-            endStr
+            messageStart,
+            messageEnd
 
         options.forEach((elem) => {
             let reg = new RegExp(`(http|https)://.*.${elem}`, 'gi')
             if (reg.test(str)) {
-                res = str.match(reg)
-                index = str.indexOf(res[0])
-                startSrt = str.slice(0, index)
-                endStr = str.slice(res[0].length + index)
+                image = str.match(reg)
+                index = str.indexOf(image[0])
+                messageStart = str.slice(0, index)
+                messageEnd = str.slice(image[0].length + index)
             }
         })
 
-        return { res, startSrt, endStr }
+        return { image, messageStart, messageEnd }
     }
 
-    const ViewDialogueResult = () => {
+    const ViewChatResult = () => {
         return (
-            <div className="Dialogue__result">
+            <div className="Chat__result">
                 <div className="stars">
-                    <CalcStars element={dialogueResultObj} property='grade' iconSize='3' />
+                    <CalcStars element={chatResult} property='grade' iconSize='3' />
                 </div>
                 <p>
                     Диалог завершился &nbsp;
-                    {moment(dialogueResultObj.completionTime).fromNow()}
+                    {moment(chatResult.completionTime).fromNow()}
                 </p>
             </div>
         )
     }
 
-    const ViewMessages = ({ messageArray }) => {
-        messageArray.reverse()
-        return messageArray.map((elem) => {
+    const ViewMessages = ({ chatMessages }) => {
+        chatMessages.reverse()
+        return chatMessages.map((elem) => {
             let timestamp = calculateDate(elem.time)
-            let { res, startSrt, endStr } = analyzeContent(elem.content)
+            let { image, messageStart, messageEnd } = getImageConfig(elem.content)
             return (
                 <Toast
                     className={elem.isOperator ? 'operator-message' : 'client-message'}
@@ -126,39 +123,32 @@ function Chat({ chats, updateChatsInDB, settingsUser, pushNewMessage, user }) {
                         <div className="time-message">{timestamp}</div>
                     </ToastHeader>
                     <ToastBody>
-                        {res ? (
+                        {image ? (
                             <>
-                                <div>{startSrt}</div>
-                                <PicturePreview srcImg={res[0]} style={{ name: "image-message" }} />
-                                <div>{endStr}</div>
+                                <div>{messageStart}</div>
+                                <PicturePreview srcImg={image[0]} style={{ name: "image-message" }} />
+                                <div>{messageEnd}</div>
                             </>
-                        ) : elem.content
-                        }
+                        ) : elem.content}
                     </ToastBody>
                 </Toast>
             )
         })
     }
 
-    const dialogueResult = dialogueResultObj.completionTime ? <ViewDialogueResult /> : null
-
-    const messages = messageArray.length > 0 ? <ViewMessages messageArray={messageArray} /> : null
-
     return (
         <>
             <ToastContainer />
-            <div className="Dialogue">
-                <div className="Dialogue__client">
+            <div className="Chat">
+                <div className="Chat__client">
                     {clientName}
-                    {isSavedCase ? (
+                    {isSavedChat ? (
                         <Button
                             type="button"
                             outline
                             color="danger"
                             size="sm"
-                            onClick={() => {
-                                updateChatsInDB({ isSave: false }, itemId)
-                            }}
+                            onClick={() => updateChatsInDB({ isSave: false }, itemId)}
                         >
                             Удалить из сохранённых
                         </Button>
@@ -167,31 +157,27 @@ function Chat({ chats, updateChatsInDB, settingsUser, pushNewMessage, user }) {
                             outline
                             color="primary"
                             size="sm"
-                            onClick={() => {
-                                updateChatsInDB({ isSave: true }, itemId)
-                            }}
+                            onClick={() => updateChatsInDB({ isSave: true }, itemId)}
                         >
                             Сохранить диалог
                         </Button>
                     )}
                 </div>
-                <div className="Dialogue__messages">
+                <div className="Chat__messages">
+                    {loading ? <Spinner /> : null}
                     <div className="messages">
-                        {messages}
+                        {chatMessages.length > 0 ? <ViewMessages chatMessages={chatMessages} /> : null}
                     </div>
                 </div>
-                {dialogueResult}
+                {chatResult.completionTime ? <ViewChatResult /> : null}
                 <OperatorAnswer itemId={itemId} />
             </div>
         </>
     )
 }
 
-const mapStateToProps = ({ chats, settingsUser, user }) => ({ chats, settingsUser, user })
+const mapStateToProps = ({ chats, settingsUser, user, loading }) => ({ chats, settingsUser, user, loading })
 
-const mapDispatchToProps = {
-    updateChatsInDB,
-    pushNewMessage
-}
+const mapDispatchToProps = { updateChatsInDB, pushNewMessage }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chat)
